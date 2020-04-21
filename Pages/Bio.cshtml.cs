8b24f003739045106e209bebeea3d0bf9f866373
William Shivers <tayloe93@gmail.com>
2020-04-21 13:35:08 -0500

    Add project files.

---
+++ Pages/Bio.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JennyLeePhotography.Pages
+{
+    public class BioModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
